f58583c80e571397768fedc958a941e09ec3885f
Akshay Shetty <akshashe@Akshays-MacBook-Air.local>
2025-02-28 12:16:53 -0800

    changes made

--- task-manager-backend/src/db/db.ts
+++ task-manager-backend/src/db/db.ts
@@ -6,12 +6,19 @@ dotenv.config();
 // Parse the DATABASE_URL
 const connectionString = process.env.DATABASE_URL;
 
+// export const pool = new Pool({
+//   user: 'postgres',
+//   host: 'db.jniuqtxxemrvjnpbhopt.supabase.co', // Use a valid hostname or IPv4 address
+//   database: 'postgres',
+//   password: 'Grep{!8923!}',
+//   port: 5432,
+//   ssl: {
+//     rejectUnauthorized: false, // Required for Supabase
+//   },
+// });
+
 export const pool = new Pool({
-  user: 'postgres',
-  host: 'db.jniuqtxxemrvjnpbhopt.supabase.co', // Use a valid hostname or IPv4 address
-  database: 'postgres',
-  password: 'Grep{!8923!}',
-  port: 5432,
+  connectionString,
   ssl: {
     rejectUnauthorized: false, // Required for Supabase
   },
